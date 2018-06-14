bf7ba43da33caf6bb0cc7361ad117ada38f548db
jhylands <jhh521@york.ac.uk>
2018-06-14 17:05:08 +0100

    passwords annonomised

--- cpannel.py
+++ cpannel.py
@@ -1,6 +1,7 @@
 #program to connect to the control pannel 
 
 import requests
+from payload import payload
 user_agent = "Mozilla/5.0 (X11; CrOS x86_64 10452.99.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.203 Safari/537.36"
 session_requests = requests.session()
 session_requests.headers.update({'User-Agent':user_agent})
@@ -15,4 +16,4 @@ print "key:%s"%key
 host = "81.151.241.85"
 request_url = "http://31.220.21.84:2082/" + key + "/frontend/paper_lantern/sql/addhost.html?host=" + host
 response = session_requests.get(request_url)
-print(response.content)
+#print(response.content)
