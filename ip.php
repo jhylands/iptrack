7dd959660bbc30f7fa16fed4e88823c866da2b6a
jhylands <jhh521@york.ac.uk>
2018-06-19 14:46:49 +0100

    test1

--- ip.php
+++ ip.php
@@ -1,15 +1,32 @@
 <?php
 //look up what the local definition of the ip adress was for that machine 
+$servername = "localhost";
+$username = "timepcou_iptrack";
+$password = "space(176)";
 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=timepcou_iptrack", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+/*
 query("select * from IPTrack where MachineID=? ORDER BY DateUpdated DESC",$_POST['MachineID']);
-//if($StoredMachineID==$_POST['MachineID']){
+if($StoredMachineID==$_POST['MachineID']){
     echo '1'; //no error,  no change
 }else{
     echo '2'; //no error, changed
+    query("insert into IPTrack (MachineID,IP) values($MachineID,$ip)");
 }
 
 //check the other addresses to make sure everyone is present
-query("select max(DateUpdated) from IPTrack where ... GROUP BY MachineID");
+//query("select max(DateUpdated) from IPTrack where ... GROUP BY MachineID");
 
 //if a device hasn't registered recently then alert James to this fault
-
+*/
--- ip.py
+++ ip.py
@@ -1,6 +1,28 @@
 import urllib2
 import time
 
+def getIPList():#<- this involves sql
+    pass
+
+def ping(ip):
+    pass
+
+#function which goes throught the list of IPs to decide if they have gone down yet
+def checkPeople():
+    ip_list = getIPList()
+    for ip in ip_list:
+        ping_status = ping(ip)
+        if ping_status==False:
+            report(ip)
+        else:
+            #test passed 
+            pass
+    #do something with the report?
+    
+
+
+
 while True:
         urllib2.urlopen("http://timep.co.uk/task.php?set=1").read()
         time.sleep(3600)
+
